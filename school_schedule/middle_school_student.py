7e49f99291cc9f06f1dfb9137c0b36badab04ac6
La Tasha Pollard <latashajpollard@gmail.com>
2023-04-05 13:26:19 -0500

    defines summary in middle school student class

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,22 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+    
+    def summary(self):
+        student_summary = super().summary()
+        transportation_status = self.gets_transportation
+        return f"{student_summary}. {self.student} gets transportation: {transportation_status}"
+        
+
+
+
+# Update the summary method to include information about the student's transportation status
+# Include tests for the additional functionality
+# There is one test provided (currently commented out)
+# Uncomment the test and implement the MiddleSchoolStudent class so that it passes
+# If there is time, implement additional tests for the MiddleSchoolStudent class (review the HighSchoolStudent class for ideas)
